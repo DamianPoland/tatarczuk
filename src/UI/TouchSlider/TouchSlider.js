e7e8e6d446d43dd9af92511e39c44138de2f498a
Damian <wilczynskid@o2.pl>
2021-03-19 15:46:17 +0100

    Archiwum

--- src/UI/TouchSlider/TouchSlider.js
+++ src/UI/TouchSlider/TouchSlider.js
@@ -25,35 +25,38 @@ SwiperCore.use([Autoplay, Navigation, Pagination, EffectCoverflow, EffectCube]);
 2. npm install node-sass => must reload localhost - npm start in console
 3. import { Swiper, SwiperSlide } from 'swiper/react'; import 'swiper/swiper.scss';
 4. in props push itemsArray with all slides
+5. break point is in 800px. Slider has no responsiwnes height (only width). To set responsivnes in height change in slider__wrapperMy max-width according to height (depends of aspect ratio img)
 */
 
 const TouchSlider = props => {
     return (
-        <Swiper
-            // speed={1000} //autoplay
-            // autoplay={{ delay: 1000 }} //autoplay
-            autoHeight={true}
-            initialSlide={window.innerWidth > 800 ? props.initialSlide - 1 : props.initialSlide}
-            style={{ padding: "4rem 0" }}
-            effect="coverflow" // cube or coverflow
-            spaceBetween={50} // margin
-            slidesPerView={window.innerWidth > 800 ? 3 : 1} // how many slides visible => according to with (pnone => 1 and PC => 3)
-            loop={true}
-            navigation
-            pagination={{ clickable: true }}
-        // onSlideChange={() => console.log('slide change')}
-        // onSwiper={(swiper) => console.log(swiper)}
-        >
-            {props.itemsArray.map((item, index) =>
+        <div className="slider__wrapperMy">
+            <Swiper
+                // speed={1000} //autoplay
+                // autoplay={{ delay: 1000 }} //autoplay
+                autoHeight={true}
+                initialSlide={window.innerWidth > 800 ? props.initialSlide - 1 : props.initialSlide}
+                style={{ padding: "4rem 0" }}
+                effect="coverflow" // cube or coverflow
+                spaceBetween={50} // margin
+                slidesPerView={window.innerWidth > 800 ? 3 : 1} // how many slides visible => according to with (pnone => 1 and PC => 3)
+                loop={true}
+                navigation
+                pagination={{ clickable: true }}
+            // onSlideChange={() => console.log('slide change')}
+            // onSwiper={(swiper) => console.log(swiper)}
+            >
+                {props.itemsArray.map((item, index) =>
 
-                // slider item
-                <SwiperSlide key={index} onClick={() => console.log("click: ", item)}>
-                    <div className="swiper-slider-ontainer-custom">
-                        <img className="img" src={item.img} alt={`${item}`} />
-                    </div>
-                </SwiperSlide>)}
+                    // slider item
+                    <SwiperSlide key={index} onClick={() => console.log("click: ", item)}>
+                        <div className="swiper-slider-ontainer-custom">
+                            <img className="img" src={item.img} alt={`${item}`} />
+                        </div>
+                    </SwiperSlide>)}
 
-        </Swiper>
+            </Swiper>
+        </div>
     )
 }
 
